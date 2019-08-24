ced2891388057ea5082354677b803ffa46b6aaae
Huw Jones <huwjones025@gmail.com>
2019-08-24 19:53:34 +0100

    adds gitignore

---
+++ app.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const OpenTok = require('opentok');
+const app = express();
+const cors = require('cors');
+const {key, secret} = require('./config.js');
+app.use(cors());
+const apiKey = process.env.API_KEY || key;
+const apiSecret = process.env.API_SECRET || secret;
+
+// Verify that the API Key and API Secret are defined
+if (!apiKey || !apiSecret) {
+  console.log('You must specify API_KEY and API_SECRET environment variables');
+  process.exit(1);
+}
+
+const PORT = process.env.PORT || 3000;
+
+// Starts the express app
+app.listen(PORT, function () {
+  console.log('listening on:', PORT);
+});
+
+// Initialize OpenTok
+const opentok = new OpenTok(apiKey, apiSecret);
+
+const promisifiedOpenTok = () => {
+  return new Promise((resolve, reject) => {
+    opentok.createSession(function (err, session) {
+      if (err) reject(err)
+      else resolve(session.sessionId)
+    });
+  })
+}
+
+app.get('/', function (req, res) {
+  promisifiedOpenTok()
+    .then(sessionId => {
+      const token = opentok.generateToken(sessionId);
+      res.status(200).send({ sessionId, token })
+    })
+    .catch(err => {throw err})
+});
